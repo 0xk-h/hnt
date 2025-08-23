f25d9407468ab58fde3b7496fe9d5048a0a72a37
Kishore <kishore.j21.01@gmail.com>
2025-08-23 23:15:17 +0530

    feat: initialize clap && add random generator

--- src/games/guess.rs
+++ src/games/guess.rs
@@ -1,4 +1,16 @@
 
-pub fn guess() {
-    println!("Guess the number!");
+pub fn guess(number: Option<i32>) {
+    let secret = rand::random_range(1..=10);
+
+    if let Some(number) = number {
+        println!("You guessed the number: {}", number);
+        println!("The secret number is: {}", secret);
+        if number == secret {
+            println!("You win!🎉");
+        } else {
+            println!("You lose!");
+        }
+    } else {
+        println!("No number was provided.");
+    }
 }
