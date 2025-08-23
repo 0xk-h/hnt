f3ce529df6ba7c0e922760a15973b06778ebc4ee
Kishore <kishore.j21.01@gmail.com>
2025-08-23 23:15:17 +0530

    feat: initialize clap && add random generator

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,33 @@
+use clap::{Parser, Subcommand};
 mod games;
 
 fn main() {
-    println!("Hello, world!");
-    games::guess::guess();
+    #[derive(Parser)]
+    #[command(author, version, about)]
+    struct Cli {
+        #[command(subcommand)]
+        command: Commands,
+    }
+
+    #[derive(Subcommand)]
+    enum Commands {
+        // guessing game
+        Guess {
+            number: Option<i32>,
+            q: Option<String>,
+        },
+    }
+
+    let cli = Cli::parse();
+
+    match cli.command {
+        Commands::Guess { number, q } => {
+            if let Some(_) = q {
+                println!("Quitting game");
+            } else {
+                games::guess::guess(number);
+            }
+        }
+    }
+    println!("The End");
 }
