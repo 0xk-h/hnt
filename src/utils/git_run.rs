e877d555cc4b16f1621570bf4c56b0416ce7b3ef
Kishore <kishore.j21.01@gmail.com>
2025-08-28 23:06:47 +0530

    fix: unnecessary error msg while running git

--- src/utils/git_run.rs
+++ src/utils/git_run.rs
@@ -1,14 +1,10 @@
 use std::process::{Command, Stdio};
 
 pub fn run_git(args: &[&str]) {
-    let status = Command::new("git")
+    Command::new("git")
         .args(args)
         .stdout(Stdio::inherit())
         .stderr(Stdio::inherit())
         .status()
         .expect("Unexpected error occured while running git command");
-
-    if !status.success() {
-        eprintln!("❌ Git command failed: git {:?}", args);
-    }
 }
