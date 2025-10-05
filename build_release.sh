78d6365f5e25075c4377b0827a147ec38e2e0fff
kishore399 <kishore.j21.01@gmail.com>
2025-10-05 21:18:31 +0530

    feat: simplify build script

--- build_release.sh
+++ build_release.sh
@@ -1,25 +1,29 @@
 #!/usr/bin/env bash
 set -e
 
-TARGETS=(
-    "x86_64-unknown-linux-gnu:hnt-linux-x86_64"
-    #"aarch64-unknown-linux-gnu:hnt-linux-arm64"
-    # "x86_64-apple-darwin:hnt-darwin-x86_64"
-    # "aarch64-apple-darwin:hnt-darwin-arm64"
-    #"x86_64-pc-windows-gnu:hnt-windows-x86_64.exe"
-)
+#"x86_64-unknown-linux-gnu:hnt-linux-x86_64"
+#"aarch64-unknown-linux-gnu:hnt-linux-arm64"
+# "x86_64-apple-darwin:hnt-darwin-x86_64"
+# "aarch64-apple-darwin:hnt-darwin-arm64"
+#"x86_64-pc-windows-gnu:hnt-windows-x86_64.exe"
+
 
 mkdir -p releases
 
-for t in "${TARGETS[@]}"; do
-    TARGET=${t%%:*}
-    NAME=${t##*:}
-    echo "Building $NAME ..."
-    cargo build --release --target $TARGET
+t = "x86_64-unknown-linux-gnu:hnt-linux-x86_64"
 
-    BINARY_FILE="hnt"
-    [[ $TARGET == *windows* ]] && BINARY_FILE="hnt.exe"
+TARGET=${t%%:*}
+NAME=${t##*:}
+echo "Building $NAME ..."
+cargo build --release --target $TARGET
 
-    cp target/$TARGET/release/$BINARY_FILE releases/$NAME
-done
-echo "✅ Succesfully built the releases"
+
+BINARY_FILE="hnt"
+
+cp target/$TARGET/release/$BINARY_FILE releases/$NAME
+
+chmod +x releases/$NAME
+sudo cp releases/$NAME "/usr/local/bin/hnt"
+
+
+echo "Succesfully built the releases"
