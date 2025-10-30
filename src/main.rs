c3482220afd8bb374a67ca95bf645e5f8df3d034
kishore399 <kishore.j21.01@gmail.com>
2025-10-30 17:57:25 +0530

    chore: upgrade to version 2.0.0

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ async fn main() {
     #[command(
         name = "hnt",
         author = "Kishore Kumar",
-        version = "1.7.3",
+        version = "2.0.0",
         about = "Dev productivity cli tool"
     )]
     struct Cli {
